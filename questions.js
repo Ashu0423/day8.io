0e1a2f4000c95995a42ba09c8b07acc004d95ad9
Ashu0423 <88968255+Ashu0423@users.noreply.github.com>
2021-08-24 08:21:24 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,168 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Where are the Whitsunday Islands located?",
+    answer: "Australia",
+    options: [
+      "Malaysia",
+      "Vietnam",
+      "Philippines",
+      "Australia"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of these countries ISN’T landlocked?",
+    answer: "Turkey",
+    options: [
+      "Nepal",
+      "Turkey",
+      "Armenia",
+      "Austria"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many provinces are there in Canada?",
+    answer: "10",
+    options: [
+      "5",
+      "10",
+      "20",
+      "30"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which two countries in South America are the Iguazu Falls part of?",
+    answer: "Brazil and Argentina",
+    options: [
+      "Ethiopia and Kenya",
+      "Switzerland and Italy",
+      "Brazil and Argentina",
+      "Tanzania and Zambia"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The Aruba Caribbean island is a territory of which European country?",
+    answer: "Netherlands",
+    options: [
+      "Oman",
+      "Netherlands",
+      "Finland",
+      "Croatia"
+    ]
+  },
+    {
+        numb:6,
+        question:"What is the capital of Canada?",
+        answer:"Ottawa",
+        options:[
+            "Toronto",
+            "Vancouver",
+            "Ottawa",
+            "Halifax"
+        ]
+    },
+    {
+    numb:7,
+    question : "Hanoi is the capital of which country?",
+    answer:"Vietnam",
+    options:[
+        "Cambodia",
+        "Laos",
+        "Thailand",
+        "Vietnam"
+    ]
+},
+{
+    numb:8,
+    question:"Which superhero movie was Niagara Falls used in?",
+    answer:"Superman II",
+    options:[
+        "Batman",
+        "Gladiator",
+        "Fast and the Furious",
+        "Superman II"
+    ]
+},
+{
+    numb:9,
+    question:"How many countries connect to the Austrian border?",
+    answer:"8",
+    options:[
+        "4",
+        "8",
+        "12",
+        "15"
+    ]
+},
+{
+    numb:10,
+    question:"LOT is the national airline of which European country?",
+    answer:"Poland",
+    options:[
+        "Italy",
+        "Latvia",
+        "China",
+        "Poland"
+    ]
+},
+{
+    numb:11,
+    question:"The Baltic States refer to which countries?",
+    answer:"Estonia, Latvia, Lithuania",
+    options:[
+        "Iceland, Norway, UK",
+        "Poland, Czech Republic, Austria",
+        "Estonia, Latvia, Lithuania",
+        "Croatia, Slovenia, Italy"
+    ]
+},
+{
+    numb:12,
+    question:"Bohemian Switzerland National Park is in which country?",
+    answer:"Czech Republic",
+    options:[
+        "Germany",
+        "Netherlands",
+        "Czech Republic",
+        "France"
+    ]
+},
+{
+    numb:13,
+    question:"How high is Mount Everest?",
+    answer:"8,848 m",
+    options:[
+        "1,550 m",
+        "2,900 m",
+        "8,848 m",
+        "25,600 m"
+    ]
+},
+{
+    numb:14,
+    question:"What is the currency of Mongolia?",
+    answer:"Tugrik",
+    options:[
+        "Yena",
+        "Kirgin",
+        "Topchok",
+        "Tugrik"
+    ]
+},
+{
+    numb:15,
+    question:"Which country has more lakes than the rest of the world combined?",
+    answer:"Canada",
+    options:[
+        "Finland",
+        "China",
+        "Norway",
+        "Canada"
+    ]
+},
+
+];
